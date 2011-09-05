624cfff628e0cda66a0cc88a46385645be372207
Dave Simons <dave@inuits.be>
2011-09-05 22:44:03 +0200

    virt pkgs, realize server only when needed
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,10 +1,16 @@
 class munin::packages {
-	package {
+	@package {
 		'munin':
-			ensure => present;
-#			require => Package['apache'];
+			ensure => present,
+			require => Package["$webserver"];
 
 		'munin-node':
 			ensure => present;
+	}
+
+	realize(Package['munin-node'])
+
+	if $munin::server == yes {
+		realize(Package['munin'])
 	}
 }
