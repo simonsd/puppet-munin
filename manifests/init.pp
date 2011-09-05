be6065d8d198581859b0feddb9fd1e70b13773d4
Dave Simons <dave@inuits.be>
2011-09-05 22:43:35 +0200

    update inclusion syntax, add params
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,17 @@ import 'packages.pp'
 import 'config.pp'
 import 'services.pp'
 
-class munin {
-	include munin::packages
-	include munin::config
-	include munin::services
+class munin (
+	$server = 'no',
+	$webserver = $::operatingsystem ? {
+		default => 'httpd',
+		debian => 'apache',
+	}
+) {
+	class {
+		'munin::packages':
+			before => Class['munin::config'];
+		'munin::config':
+			before => Class['munin::services'];
+		'munin::services':;
 }
